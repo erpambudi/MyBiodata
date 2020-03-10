34a91fcd40faec06d90921ef006b51750eee3b6e
erpambudi <erpambudi76@gmail.com>
2020-03-10 20:50:35 +0700

    add intent implicit to contact

--- app/src/main/java/com/hope/mybiodata/MainActivity.java
+++ app/src/main/java/com/hope/mybiodata/MainActivity.java
@@ -2,13 +2,30 @@ package com.hope.mybiodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnContact = findViewById(R.id.btn_contact);
+        btnContact.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(R.id.btn_contact == v.getId()){
+            String phoneNumber = "08994636544";
+            Intent intentImplicitPhone = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+            startActivity(intentImplicitPhone);
+        }
     }
 }
